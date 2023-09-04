600db687ad304b9982573692527a3f77c8fe7216
RohitSengupta22 <chints.rsg@gmail.com>
2023-09-04 11:10:30 +0530

    Minor edit

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -10,7 +10,7 @@ export const verifyToken = async(req,res,next) =>{
         }
 
         if(token.startsWith("Bearer ")){
-            token = token.slice(7,toke.length).trimLeft()
+            token = token.slice(7,token.length).trimLeft()
         }
 
         const verified = jwt.verify(token,process.env.SECRET);
